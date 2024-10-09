c6daf17451f3c774d8b1ace55b199d7f18abdb01
Eridanus-ui <njigupaul22@gmail.com>
2024-10-09 15:55:14 +0300

    feat: Add newsletter subscription form
    Add a newsletter subscription form to the website.
    Allow users to enter their email address to subscribe.
    Display a success message upon successful submission.

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 
 export default function NewsletterBox() {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="text-center">
       <p className="text-2xl font-medium text-gray-800 font-imprima">
         Subscribe now & get 20% off
       </p>
+      <p className="text-gray-400 mt-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac
+        congue libero, ut semper urna.
+      </p>
+      <form
+        action=""
+        className="w-full sm:w-1/2 flex items-center gap-3 my-6 mx-auto border pl-3"
+      >
+        <input
+          className="w-full sm:flex-1 outline-none font-muktaVaani"
+          type="email"
+          name="email"
+          id=""
+          placeholder="Enter your email"
+          required
+        />
+        <button
+          onClick={onSubmitHandler}
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4 font-muktaVaani"
+        >
+          SUBSCRIBE
+        </button>
+      </form>
     </div>
   );
 }
