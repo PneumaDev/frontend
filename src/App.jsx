1071df7fde456a48719d1e3ecad2d819d92d243d
Eridanus-ui <njigupaul22@gmail.com>
2024-10-09 18:10:49 +0300

    feat: Add footer component
    Introduce a new Footer component for displaying website information
    and navigation links at the bottom of each page.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -26,6 +27,7 @@ export default function App() {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+export default function Footer() {
+  return (
+    <div className="">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div className="">
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 font-imprima">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque ac congue libero, ut semper urna.
+          </p>
+        </div>
+        <div className="">
+          <p className="text-xl font-medium mb-5 font-muktaVaani">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li className="font-yantramanav">Home</li>
+            <li className="font-yantramanav">About Us</li>
+            <li className="font-yantramanav">Delivery</li>
+            <li className="font-yantramanav">Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="">
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li className="">
+              <a href="tel:+254704777637" className="font-yantramanav">
+                (+254) 704 777 637
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:support@eridanus.com"
+                className="font-yantramanav"
+              >
+                support@eridanus.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <footer className="mt-auto py-6">
+        <div className="container mx-auto px-4">
+          <hr className="border-gray-300 mb-4" />
+          <p className="text-sm text-center text-gray-600">
+            &copy; {new Date().getFullYear()} Eridanus.com - All Rights Reserved
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
