0f5f60773ec91f0ca2db1b5f7c3fc36b26b95943
Eridanus-ui <njigupaul22@gmail.com>
2024-10-14 17:12:16 +0300

    feat: Implement product page component
    Add a dedicated product page component to display detailed information about individual products.
    Enhance the user experience by providing a dynamic image gallery and size selection option.
    Implement a clean and visually appealing layout for the product page.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,102 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 
 export default function Product() {
-  return <div>Product</div>;
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((product) => {
+      if (product._id === productId) {
+        setImage(product.image[0]);
+        setProductData(product);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Products images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex scroller sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((image, index) => (
+              <img
+                onClick={() => setImage(image)}
+                key={index}
+                src={image}
+                alt=""
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt="" className="w-full h-auto" />
+          </div>
+        </div>
+
+        {/* Product info */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2 font-muktaVaani">
+            {productData.name}
+          </h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className="pl-2 font-imprima">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium font-yantramanav">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5 font-imprima">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p className="font-yantramanav">Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border font-imprima py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black font-muktaVaani text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p className="font-muktaVaani">100% Original Product</p>
+            <p className="font-muktaVaani">
+              Cash on delivery is available for this product.
+            </p>
+            <p className="font-muktaVaani">
+              Easy return and exchange policy within 7 days.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 }
