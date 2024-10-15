75386af6236e6854572e78fe034b89ff1e88e310
Eridanus-ui <njigupaul22@gmail.com>
2024-10-15 19:33:49 +0300

    feat: Implement cart data management
    Add logic to retrieve and display cart items
    Store cart data in state for easier access and updates
    Prepare for future cart functionality implementation

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 export default function Cart() {
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: item,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+
+    console.log(tempData);
+  }, [cartItems]);
   return <div>Cart</div>;
 }
