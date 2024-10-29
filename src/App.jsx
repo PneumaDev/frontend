c48ef7daaccb27b3fe0094138f7dcbf441038b7e
Eridanus-ui <njigupaul22@gmail.com>
2024-10-29 20:57:37 +0300

    feat: Add loading spinner for lazy loaded components
    Improve user experience by showing a loading spinner while
    lazy loaded components are being fetched.
    This provides a more seamless and informative loading
    process.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Collection from "./pages/Collection";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Product from "./pages/Product";
-// import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
-// import PlaceOrder from "./pages/PlaceOrder";
-// import Orders from "./pages/Orders";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import SearchBar from "./components/SearchBar";
 import { Toaster } from "react-hot-toast";
-// import NotFound from "./pages/NotFound";
+import Spinner from "./components/Spinner";
 
 const Home = lazy(() => import("./pages/Home"));
 const Collection = lazy(() => import("./pages/Collection"));
@@ -39,18 +27,20 @@ export default function App() {
         }}
       />
       <SearchBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
