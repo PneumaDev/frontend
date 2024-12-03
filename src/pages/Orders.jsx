97f7e5ede7213206dc87f908117e479f69a2c056
Eridanus-ui <njigupaul22@gmail.com>
2024-12-03 22:28:02 +0300

    feat: Enhance order management UI
    Improve the visual presentation of order details.
    Add a new component for displaying individual order items.
    Enhance functionality for cancelling and tracking orders.

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,8 +5,9 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import Spinner from "./../components/Spinner";
 import Modal from "../components/Modal";
-import { Copy } from "lucide-react";
+import { Copy, RefreshCw } from "lucide-react";
 import InfoMessage from "../components/InfoComponent";
+import OrderItem from "../components/OrderItem";
 
 export default function Orders() {
   // <------Import Context Variables----->
@@ -21,7 +22,7 @@ export default function Orders() {
   const [currentItem, setCurrentItem] = useState(selectedItem);
   const [action, setAction] = useState(null);
 
-  // <------------Handle Side Effects------------>
+  // <-------------------Handle Side Effects---------------->
   useEffect(() => {
     fetchData();
   }, [token]);
@@ -32,7 +33,7 @@ export default function Orders() {
     }
   }, [orderData]);
 
-  // <------------Custom Functions------------>
+  // <-------------------Custom Functions--------------------->
   const handleTrackOrder = async (e, item) => {
     //Function to track orders
     setAction("track_order");
@@ -45,8 +46,8 @@ export default function Orders() {
     setSendingData(false);
   };
 
-  // Load Order Data
   const loadOrderData = async () => {
+    // Load Order Data
     try {
       if (!token) {
         return null;
@@ -70,8 +71,8 @@ export default function Orders() {
     }
   };
 
-  // Proceed to Complete Stalled Purchases
   const completePurchases = async () => {
+    // Proceed to Complete Stalled Purchases
     if (selectedItem) {
       try {
         const orderId = selectedItem._id;
@@ -98,8 +99,8 @@ export default function Orders() {
     setOpenModal(true);
   };
 
-  // <--------------Cancel/Delete Item-------------->
   const cancelOrder = async (orderId) => {
+    // Cancel/Delete Item
     setLoading(true);
     try {
       const response = await axios.post(
@@ -135,196 +136,51 @@ export default function Orders() {
     setLoading(false);
   };
 
+  const calculateTimePassed = (orderTime) => {
+    const updatedAt = new Date(orderTime);
+    const currentTime = new Date();
+    return Math.floor((currentTime - updatedAt) / 1000);
+  };
+
   return (
     <div className="border-t pt-16 bg-white">
       <div className="text-2xl">
-        <Title text1={"MY"} tex2={"ORDERS"} />
+        <Title text1="MY" tex2="ORDERS" />
       </div>
-      <div className="">
-        {loading ? (
-          <>
-            <Spinner />
-          </>
-        ) : orderData.length === 0 ? (
-          <>
-            <div className="flex justify-center items-center">
-              <InfoMessage
-                title={"Nothing Here!"}
-                message={"Please make some orders!"}
-              />
-            </div>
-          </>
-        ) : (
-          <>
-            <>
-              <div className="space-y-6">
-                {orderData.map((order, index) => (
-                  <div
-                    key={index}
-                    className="p-6 border rounded-lg shadow-sm bg-white hover:shadow-md transition-shadow space-y-4"
-                  >
-                    <div className="flex justify-between items-start">
-                      <div className="space-y-1">
-                        <p className="text-sm text-gray-500 font-yantramanav font-semibold ">
-                          ID:{" "}
-                          <span className="font-muktaVaani">{order._id}</span>
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          Placed on {new Date(order.date).toLocaleDateString()}
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          Payment Method: {order.paymentMethod.toUpperCase()}
-                        </p>
-                      </div>
-                      <div
-                        className={`px-3 py-1 rounded-full text-sm font-medium font-yantramanav ${
-                          order.status === "Delivered"
-                            ? "bg-green-100 text-green-800"
-                            : order.status === "Shipped"
-                            ? "bg-blue-100 text-blue-800"
-                            : order.status === "Packing"
-                            ? "bg-orange-100 text-orange-600"
-                            : order.status === "Pending"
-                            ? "bg-orange-100 text-orange-800"
-                            : "bg-red-100 text-red-800"
-                        }`}
-                      >
-                        {order.status}
-                      </div>
-                    </div>
-
-                    {order.items.map((item, idx) => (
-                      <div
-                        key={idx}
-                        className="flex items-center space-x-4 py-4 border-b last:border-b-0"
-                      >
-                        <img
-                          src={item.image[0]}
-                          alt={item.name}
-                          className="w-20 h-28 object-cover aspect-auto rounded-md"
-                        />
-                        <div className="flex-1 space-y-2">
-                          <p className="font-medium text-gray-700 line-clamp-1 font-muktaVaani">
-                            {item.name}
-                          </p>
-                          <div className="flex items-center text-sm text-gray-500 font-yantramanav">
-                            <p>Qty: {item.quantity}</p>
-                            <span className="mx-2">•</span>
-                            <p>Size: {item.size}</p>
-                            <span className="mx-2">•</span>
-                            <p>KSH {item.price.toLocaleString()}</p>
-                          </div>
-                          <p className="text-sm text-gray-500 font-imprima line-clamp-2">
-                            {item.description}
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-
-                    <div className="flex justify-between items-start pt-4">
-                      <div className="space-y-1 ">
-                        <p className="font-medium text-gray-800 font-muktaVaani">
-                          Delivery Details:
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          {order.address.firstName} {order.address.lastName}
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          {order.address.phone}
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          {order.address.email}
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          {order.address.street}
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav">
-                          {order.address.city}, {order.address.county}
-                        </p>
-                      </div>
-                      <div className="text-right">
-                        <p className="text-sm text-gray-500 font-muktaVaani">
-                          Total Amount:
-                        </p>
-                        <p className="text-lg font-medium text-gray-800">
-                          KSH {order.amount.toLocaleString()}
-                        </p>
-                        <p className="text-sm text-gray-500 font-yantramanav mt-1">
-                          Payment Status: {order.payment ? "Paid" : "Pending"}
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="flex gap-3 pt-4 justify-end">
-                      {order.status === "Delivered" ? (
-                        <button
-                          onClick={() => {
-                            toast.error("Feature Not Implemented", {
-                              id: "Feature Not Implemented",
-                            });
-                          }}
-                          className="border bg-blue-100 px-4 py-2 text-sm font-medium rounded-md transition-colors hover:bg-gray-200 text-gray-700 cursor-pointer"
-                        >
-                          Apply For Refund
-                        </button>
-                      ) : (
-                        <button
-                          disabled={
-                            loading ||
-                            order.payment ||
-                            order.status !== "Pending"
-                          }
-                          onClick={() => cancelOrder(order._id)}
-                          className={`border px-4 py-2 text-sm font-medium rounded-md transition-colors ${
-                            loading ||
-                            order.status !== "Pending" ||
-                            order.payment
-                              ? "opacity-50 cursor-not-allowed bg-gray-100"
-                              : "hover:bg-gray-200 bg-white text-gray-700 cursor-pointer"
-                          }`}
-                        >
-                          Cancel Order
-                        </button>
-                      )}
-
-                      {!order.payment ? (
-                        <button
-                          onClick={(e) =>
-                            completePurchasesConfirmation(e, order)
-                          }
-                          className="bg-blue-600 text-white px-4 py-2 text-sm font-medium rounded-md hover:bg-blue-700 transition-colors"
-                        >
-                          Pay Now (KSH {order.amount.toLocaleString()})
-                        </button>
-                      ) : (
-                        <button
-                          onClick={(e) => handleTrackOrder(e, order)}
-                          className="border px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-200 bg-white text-gray-700 transition-colors"
-                        >
-                          Track Order
-                        </button>
-                      )}
-                    </div>
-
-                    {order.status === "Delivered" && order.payment && (
-                      <div className="flex justify-end">
-                        {" "}
-                        <button
-                          onClick={() => handleWriteReview(order)}
-                          className="mt-4 bg-gray-900 text-white px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-800 transition-colors"
-                        >
-                          Write a Review
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </>
-          </>
-        )}
-      </div>
-
+      {loading ? (
+        <Spinner />
+      ) : orderData.length > 0 ? (
+        <div
+          className={`p-4 grid gap-8 grid-cols-1 ${
+            orderData.length == 1 ? "" : "md:grid-cols-2"
+          }`}
+        >
+          {orderData.map((order, idx) => (
+            <OrderItem
+              key={idx}
+              order={order}
+              loading={sendingData}
+              cancelOrder={cancelOrder}
+              completePurchasesConfirmation={completePurchasesConfirmation}
+              calculateTimePassed={calculateTimePassed}
+              handleTrackOrder={handleTrackOrder}
+              handleWriteReview={(item) => {
+                setCurrentItem(item);
+                setAction("writeReview");
+                setOpenModal(true);
+              }}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center">
+          <InfoMessage
+            type="info"
+            title={"Nothing Here!"}
+            message={"Please make some orders then comeback!"}
+          />
+        </div>
+      )}
       <div className="">
         {action === "track_order" ? (
           <>
