5fc292c804b29afd68305b92aad7c41e6515b7a2
Eridanus-ui <njigupaul22@gmail.com>
2025-01-07 09:20:20 +0300

    style(Footer): adjust Footer top margin
    ```

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets";
 export default function Footer() {
   return (
     <div className="">
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-28 md:mt-40 text-sm">
         <div className="">
           <img src={assets.logo} className="mb-5 w-32" alt="" />
           <p className="w-full md:w-2/3 text-gray-600 font-imprima">
