18775c3454e2cb093523fd0c6affa81de05da213
Eridanus-ui <njigupaul22@gmail.com>
2025-02-10 09:28:10 +0300

    ⚡️ fetch products data on home page load

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import OurPolicy from "../components/OurPolicy";
 import NewsletterBox from "../components/NewsletterBox";
 import BestSeller from "../components/BestSeller";
+import { ShopContext } from "../context/ShopContext";
 
 export default function Home() {
+  const { getProductsData } = useContext(ShopContext);
+  useEffect(() => {
+    getProductsData();
+  }, []);
+
   return (
     <div>
       <Hero />
