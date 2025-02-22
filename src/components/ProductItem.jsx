7a11724501c730bafe2a12a1b87a9843688a41fa
Eridanus-ui <njigupaul22@gmail.com>
2025-02-22 13:40:08 +0300

    💄 Adjust product item image size

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -17,7 +17,7 @@ export default function ProductItem({ id, image, price, name }) {
     .image(publicId)
     .format("auto")
     .quality("auto")
-    .resize(scale().width(200));
+    .resize(scale().width(300));
 
   return (
     <div className="relative text-gray-700 shadow-sm hover:shadow-lg p-4 rounded-md">
