ee77b196abc31f15b1a52b5d800ead2c1c3b50cc
PneumaDev <njigupaul22@gmail.com>
2025-05-15 09:01:21 +0300

    💄 adjust ProductItem image height for better layout

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -25,20 +25,22 @@ export default function ProductItem({ id, image, price, name }) {
         <div className="overflow-hidden relative">
           {!isImageLoaded && (
             <div className="flex">
-              <div className="w-[300px] h-[200px] md:h-[200px] bg-gray-200 animate-pulse rounded-md"></div>
+              <div className="w-[300px] h-[200px] md:h-[320px] bg-gray-200 animate-pulse rounded-md"></div>
             </div>
           )}
 
           {/* Use AdvancedImage to display the optimized image */}
-          <AdvancedImage
-            plugins={[lazyload()]}
-            cldImg={cldImg}
-            alt={name}
-            className={`hover:scale-110 transition ease-in-out rounded-md hover:rounded-md ${
-              isImageLoaded ? "opacity-100" : "opacity-0"
-            }`}
-            onLoad={() => setIsImageLoaded(true)}
-          />
+          {isImageLoaded && (
+            <AdvancedImage
+              plugins={[lazyload()]}
+              cldImg={cldImg}
+              alt={name}
+              className={`hover:scale-110 transition ease-in-out rounded-md hover:rounded-md ${
+                isImageLoaded ? "opacity-100" : "opacity-0"
+              }`}
+              onLoad={() => setIsImageLoaded(true)}
+            />
+          )}
 
           <BadgeCheck
             className="absolute z-10 top-2 right-2 cursor-pointer"
